fc21f0eb895e386fc27097b758ed29c6a52e25b7
qznikk <115718297+qznikk@users.noreply.github.com>
2025-05-17 15:01:34 +0200

    Add RequestBox component and update Requests and SubmitIssue pages

--- frontend/src/app/submit-issue/page.tsx
+++ frontend/src/app/submit-issue/page.tsx
@@ -1,45 +1,69 @@
-"use client"
+"use client";
 import "../globals.css";
 import { useState, useEffect } from "react";
 
 //Tenants Only
 function SubmitIssue() {
-    const [ready,setReady] = useState(false)
-    const [error, setError] = useState('none')
+  const [ready, setReady] = useState(false);
+  const [error, setError] = useState("none");
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
 
-    useEffect(() => {
-        //Page setup goes here
-        setReady(true);
-    },[])
+  const handleSubmit = () => {
+    //handle api
 
-    if(ready)
-    {
-        if(error == 'none')
-        {
-            return (
-                <main>
-                    <h1>issue submit page</h1>
-                </main>
-            );
-        }
-        else
-        {
-            return (
-                <main>
-                    <b>An error has occured:</b>
-                    <h1>{error}</h1>
-                </main>
-            );
-        }
+    console.log("Submitted:", { title, description });
+    alert("Issue submitted!");
+  };
+
+  useEffect(() => {
+    //Page setup goes here
+    setReady(true);
+  }, []);
+
+  if (ready) {
+    if (error == "none") {
+      return (
+        <main className="flex flex-col items-center justify-start gap-6 mt-10">
+          <div className="page-head w-[50%]">
+            <b className="text-4xl">Please describe your issue here</b>
+          </div>
+
+          <input
+            type="text"
+            placeholder="Title....."
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className="w-[50%] px-4 py-2 rounded border border-gray-300 shadow-sm bg-white"
+          />
+
+          <textarea
+            placeholder="Describe the issue in detail..."
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            className="w-[50%] h-[300px] p-4 rounded border border-gray-300 shadow-sm bg-white"
+          />
+
+          <button onClick={handleSubmit} className="black-button">
+            Submit Issue
+          </button>
+        </main>
+      );
+    } else {
+      return (
+        <main>
+          <b>An error has occured:</b>
+          <h1>{error}</h1>
+        </main>
+      );
     }
-    else
-    {
-        return(
-            <main>
-                <h1>Loading...</h1>
-            </main>
-        )
-    }
-};
+  } else {
+    return (
+      <main>
+        <h1>Loading...</h1>
+      </main>
+    );
+  }
+}
 
 export default SubmitIssue;
