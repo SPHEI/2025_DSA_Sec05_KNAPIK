8ec2eaf4c1301c09ed89dfb1f6159b2e89cb806e
SPHI <115626927+SPHEI@users.noreply.github.com>
2025-06-23 13:16:00 +0200

    Tenant box controls

--- frontend/src/app/tenants/page.tsx
+++ frontend/src/app/tenants/page.tsx
@@ -10,6 +10,7 @@ function Tenants() {
     const [ready,setReady] = useState(false)
     const [error, setError] = useState('none')
     const [names,setNames] = useState([{id: -1, name: '', email: '', phone: '', role_id: -1, id_2: -1, name_2: '', price: '', status: ''}])
+    const [apartaments,setApartaments] = useState([{id: -1,name: '', street: '', building_number: '', building_name: '',flat_number:'',owner_id:-1 }])
     const pathname = usePathname();
     const router = useRouter();
     useEffect(() => {
@@ -20,6 +21,16 @@ function Tenants() {
     {
         try {
             var t = Cookies.get("token");
+            const res2 = await fetch('http://localhost:8080/apartament/list?token=' + t)
+            const data2 = await res2.json();
+            if(data2.message)
+            {
+                setError(data2.message)
+            }
+            else
+            {
+                setApartaments(data2);
+            }
             const res = await fetch('http://localhost:8080/tenant/list?token=' + t)
             const data = await res.json();
             //alert(JSON.stringify(data));
@@ -50,6 +61,64 @@ function Tenants() {
         }
     }
 
+    const changeRent = async (id: number, newRent: number) => 
+    {
+        console.log(id + " " + newRent)
+        var t = Cookies.get("token");
+        try {
+            const res = await fetch('http://localhost:8080/changerent',{
+                method:'POST',
+                body: JSON.stringify({ 
+                    "token" : t,
+                    "rent":{
+                    "apartment_id" : id,
+                    "price": newRent
+                    }
+                })
+            });
+            if(res.ok)
+            {
+                console.log("Rent changed succesfully.");
+            }
+            else
+            {
+                var data = await res.json()
+                console.log(data.message)
+            }
+        } catch (err: any) {
+            console.log(err.message)
+        } finally{
+            refresh()
+        }
+    }
+
+    const changeApartment = async(a_id: number, u_id: number, date: string) =>
+    {
+        console.log(a_id + " " + u_id + " " + date)
+        var t = Cookies.get("token");
+        const res2 = await fetch('http://localhost:8080/renting/start',{
+            method:'POST',
+            body: JSON.stringify({ 
+                "token": t,
+                "renting" : {
+                    "apartment_id" : a_id,
+                    "user_id" : u_id,
+                    "start_date" : date + "T00:00:00Z"
+                }
+            })
+        });
+        if(res2.ok)
+        {
+            console.log("renting start succesful")
+            refresh()
+        }
+        else
+        {
+            var data2 = await res2.json()
+            alert(data2.message)
+        }
+    }
+
     async function evict(id: number)
     {
         alert(id)
@@ -67,7 +136,8 @@ function Tenants() {
                     </div>
                     {names.map((text, index) => <TenantBox key={index} id={text.id} name={text.name} email={text.email} phone={text.phone} role_id={text.role_id} 
                     apartment_id = {text.id_2} apartment={text.name_2} rent={text.price} status="paid"
-                    evict={evict}/>)}
+                    evict={evict} changeRent={changeRent} changeApartment={changeApartment}
+                    apartments={apartaments}/>)}
                 </main>
             );
         }
